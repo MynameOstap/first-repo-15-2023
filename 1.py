b341336f9671ba7ab97b45b0baa4d1bab131382d
MynameOstap <148582571+MynameOstap@users.noreply.github.com>
2023-11-22 22:44:32 +0200

    add 1.py in repository

---
+++ 1.py
@@ -0,0 +1,16 @@
+car = 'Audi'
+is_student = True
+is_teacher = False
+
+is_red = True
+is_blue = True
+
+print (f'car = {car}')
+
+
+print (is_student or is_teacher)
+print (is_student and is_teacher)
+print ( not is_teacher, not is_student)
+
+
+print (is_red or is_blue)
